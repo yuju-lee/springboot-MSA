5574532142b9492300145fe64a63e85c41c63e70
yuju-lee <periq23@gmail.com>
2024-07-03 23:39:20 +0900

    Feature: Assigned new Payment-service

---
+++ src/main/java/com/sparta/springmsapayment/SpringMsaPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springmsapayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsaPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMsaPaymentApplication.class, args);
+    }
+
+}
